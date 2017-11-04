76e0ae872d135b4b8f9ce6ee173889f27f766a76
Daehyeok Kim <yeriel9159@gmail.com>
2017-11-04 17:05:34 +0900

    * JSONArray 전송 및 jsp에서 데이터를 전송 받음.

---
+++ JSONExample/src/com/json/example/main/JSONArraySend.java
@@ -0,0 +1,43 @@
+package com.json.example.main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+@WebServlet("/JSONArraySend.do")
+public class JSONArraySend extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        JSONArray jsonArray = new JSONArray();
+        
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("name", "kimdaehyeok");
+        jsonObject1.put("gender", "man");
+        
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("name", "amy");
+        jsonObject2.put("gender", "woman");
+        
+        jsonArray.add(jsonObject1);
+        jsonArray.add(jsonObject2);
+        
+        jsonArray.toJSONString();
+        
+        request.setAttribute("users", jsonArray);
+        
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/receiveJSONArrayFromServer.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
